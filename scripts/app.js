25244a8a1977917294d42b75b4ab692e1cca6524
politecnico85 <politecnico85@gmail.com>
2024-05-11 14:38:54 -0500

    cambios Sunburst 2

--- scripts/app.js
+++ scripts/app.js
@@ -6,31 +6,53 @@ const svg = canvas.append("svg")
 
 var width = 500;
 var height = 500;
-var radius = Math.min(width, height);
-var color = d3.scaleOrdinal(d3.schemeCategory20b);
+var radius = Math.min(width, height) / 2 ;
+var color = d3.scaleOrdinal(d3.schemeTableau10);
 
 // primer elemento
 
-var g = d3.select('svg')
+const g = d3.select('svg')
     .attr('width', width)
     .attr('height', height)
     .append('g')
     .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
     
+const arc = d3.arc()
+        .startAngle(function (d) { return d.x0 })
+        .endAngle(function (d) { return d.x1 })
+        .innerRadius(function (d) { return d.y0 })
+        .outerRadius(function (d) { return d.y1 });
 
  // Data source
- var partition = d3.partition()
-    .size([2 * Math.PI, radius]);
+ //var partition = d3.partition()
+//    .size([2 * Math.PI, radius]);
 
-var root = d3.hierarchy(nodeData)
+//var root = d3.hierarchy(nodeData)
+//        .sum(function (d) { return d.size});
+
+
+//partirion(root);
+
+
+    
+d3.json("../data/data.json")
+    .then(data => {
+
+        const partition = d3.partition()
+        .size([2 * Math.PI, radius]);
+
+        const root = d3.hierarchy(data)
         .sum(function (d) { return d.size});
 
+        partition(root);
 
-partirion(root);
-var arc = d3.arc()
-    .startAngle(function (d) { return d.depth ? null : "none";})
-    .enter().append('path')
-    .attr("display", function (d) { return d.depth ? null : "none"; })
-    .attr("d", arc)
-    .style('stroke', '#fff')
-    .style("fill", function (d) { return color ((d.children ? d : d.parent).data.name);});
+        g.selectAll('path')
+        .data(root.descendants())
+        .enter().append('path')
+        .attr("display", function (d) { return d.depth ? null : "none"; })
+        .attr("d", arc)
+        .style('stroke', '#fff')
+        .style("fill", function (d) { return color((d.children ? d : d.parent).data.name); });
+
+
+    })    
